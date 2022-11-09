a82766008421c3336f6a209485ab1bd04a8b8e92
DavidDov <109440152+Daviddov@users.noreply.github.com>
2022-11-09 23:44:39 +0200

    Add files via upload

--- react-finel/components/pages/Albums.jsx
+++ react-finel/components/pages/Albums.jsx
@@ -1,21 +1,33 @@
+import { ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { Fragment } from "react";
 import { useMemo, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-function Albums() {
-    const [post, setPost] = useState('')
-    const { postId } = useParams();
-    const navigete = useNavigate()
-
-    console.log(post);
+function Albums({ user }) {
+    const [albums, setAlbums] = useState('')
 
     const fetchPost = useMemo(async () => {
-        const response = await fetch('https://jsonplaceholder.typicode.com/albums/' + postId)
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/albums`)
         const json = await response.json();
-        setPost(json)
+        json.sort((a, b) => (a.title > b.title) ? 1 : (a.title < b.title) ? -1 : 0) 
+        setAlbums(json)
         console.log(json)
-    }, [postId])
+    }, [])
 
-    return ( <h1>Albums</h1> );
+    return (
+        <Fragment>
+
+            <h1>Albums</h1>
+            {albums && albums.map((album, index) =>
+                <ListItem key={album.id} component="div" disablePadding sx={{ width: '100%', maxWidth: 600, bgcolor: 'background.paper' }}>
+                    <ListItemButton >
+                        <ListItemText primary={`${index+1}. ${album.title}`} />
+                    </ListItemButton>
+                </ListItem>
+
+            )}
+        </Fragment>
+    );
 }
 
 export default Albums;
